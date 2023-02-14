dbd0f721179db2c02eb764d3cdc82ed15ad83d1a
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-02-14 13:00:54 -0300

    refactor: improve accesibility by being the addOns more clickable

--- src/components/AddOn.tsx
+++ src/components/AddOn.tsx
@@ -30,31 +30,37 @@ export const AddOn: FC<props> = ({
   return (
     <div
       className={
-        "relative w-full border border-n-light-gray rounded-md p-3 lg:px-6 lg:py-3 flex gap-4 items-center hover:opacity-70 " +
+        "relative w-full border border-n-light-gray rounded-md  flex gap-4 items-center hover:opacity-70 " +
         (checked ? " border-p-purplish-blue bg-n-alabaster" : "")
       }
       data-testid={`addon-${title}-container`}
     >
-      <div className="flex gap-4">
+      <label className="flex gap-4 cursor-pointer w-full p-3 lg:px-6 lg:py-3">
         <input
           autoComplete="off"
-          className="w-6 h-6 self-center cursor-pointer"
+          className="w-6 h-6 self-center"
           data-testid={`addon-${title}`}
           defaultChecked={checked}
           id=""
           name=""
           type="checkbox"
-          onClick={() => toggleCheck(!checked)}
-          onKeyPress={(e) => {
-            e.preventDefault();
+          onClick={() => {
             toggleCheck(!checked);
+          }}
+          onKeyDown={(e) => {
+            if (e.code === " ") {
+              e.preventDefault();
+              toggleCheck(!checked);
+            } else if (e.code === "Enter") {
+              e.preventDefault();
+            }
           }}
         />
         <div className="w-8/12 sm:w-full text-left">
           <h3 className="text-base text-p-marine-blue font-bold">{title}</h3>
           <p className="text-n-cool-gray justify-self-start">{desc}</p>
         </div>
-      </div>
+      </label>
       <p className="text-p-purplish-blue text-base font-medium absolute right-2 lg:right-6">
         {type === "monthly" ? `+$${price}/mo` : `+$${price}/yr`}
       </p>
